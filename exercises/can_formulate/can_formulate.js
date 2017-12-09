adbae53493145152986f776fb9edff8936661988
alohaglenn <glengonda@gmail.com>
2017-12-08 23:46:38 -0800

    feat: add can_formulate

---
+++ exercises/can_formulate/can_formulate.js
@@ -0,0 +1,19 @@
+/*
+Given an arbitrary target string and another string of lyrics to the chorus of the song 'good kid', write a function that returns a boolean of whether or not the target string can be formulated from the lyrics.
+
+Each letter in the lyrics string can only be used once in your target string.
+
+Note: Ignore case-sensitivity.
+*/
+
+let good_kid_chorus = 'Mass hallucination baby Ill education baby Want to reconnect with your elations This is your station baby';
+
+/**
+ * @param {string} target
+ * @param {string} lyrics
+ * @return {boolean}
+ */
+
+const can_formulate = (target, lyrics) => {
+
+}
---
+++ solutions/can_formulate/can_formulate.js
@@ -0,0 +1,44 @@
+/*
+Given an arbitrary target string and another string of lyrics to the chorus of the song 'good kid', write a function that returns a boolean of whether or not the target string can be formulated from the lyrics.
+
+Each letter in the lyrics string can only be used once in your target string.
+
+Note: Ignore case-sensitivity.
+*/
+
+let good_kid_chorus = 'Mass hallucination baby Ill education baby Want to reconnect with your elations This is your station baby';
+
+/**
+ * @param {string} target
+ * @param {string} lyrics
+ * @return {boolean}
+ */
+
+const can_formulate = (target, lyrics) => {
+
+  if (target.length > lyrics.length) {
+    return false;
+  }
+
+  let target_lowercase = string_to_lowercase(target);
+  let lyrics_lowercase = string_to_lowercase(lyrics);
+
+  let possible_chars = {};
+
+  for (char of lyrics_lowercase) {
+    possible_chars[char] = (possible_chars[char] || 0) + 1;
+  }
+
+  for (char of target_lowercase) {
+    possible_chars[char] = (possible_chars[char] || 0) - 1;
+    if (possible_chars[char] < 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const string_to_lowercase = string => {
+  return string.toLowerCase();
+}
