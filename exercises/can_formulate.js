e033ff6a9e94ecb0e9f8103a2d02855ef4dd9d62
alohaglenn <glenngonda@gmail.com>
2017-12-10 00:20:26 -0800

    fix: use es6 export

--- exercises/can_formulate.js
+++ exercises/can_formulate.js
@@ -6,6 +6,7 @@ Each letter in the lyrics string can only be used once in your target string.
 Note: Ignore case-sensitivity.
 */
 
+// Example:
 let target_string = 'Hack Reactor';
 let good_kid_chorus = 'Mass hallucination baby Ill education baby Want to reconnect with your elations This is your station baby';
 
@@ -19,5 +20,4 @@ const can_formulate = (target, lyrics) => {
 
 }
 
-// module.exports = can_formulate;
 export default can_formulate;
