352ff8715d263cb435a0d6f600efad1f2f145807
alohaglenn <glenngonda@gmail.com>
2017-12-10 12:03:34 -0800

    feat: add initial test for reverse_track_order

--- __tests__/reverse_track_order.spec.js
+++ __tests__/reverse_track_order.spec.js
@@ -1,1 +1,5 @@
 import reverse_track_order from '../exercises/reverse_track_order';
+
+test('reverse_track_order is a function', () => {
+  expect(typeof reverse_track_order).toEqual('function');
+});
