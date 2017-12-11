1d909008b25a67b556085157a378af328729a9ae
alohaglenn <glenngonda@gmail.com>
2017-12-10 23:21:13 -0800

    chore: update directions for reverse_track_order

--- exercises/reverse_track_order.js
+++ exercises/reverse_track_order.js
@@ -2,9 +2,12 @@
 Eight months after "DAMN" was released, Kendrick Lamar released "DAMN COLLECTOR'S EDITION" which contained the exact same tracks, but with the album order reversed.
 
 Given an array of track names as strings, reverse the elements in the array in-place.
-*/
+
+Example:
 
 let album_tracks = ['BLOOD', 'DNA', 'YAH', 'ELEMENT', 'FEEL', 'LOYALTY', 'PRIDE', 'HUMBLE', 'LUST', 'LOVE', 'XXX', 'FEAR', 'GOD', 'DUCKWORTH'];
+reverse_track_order_in_place(album_tracks) => return ['DUCKWORTH', 'GOD', 'FEAR', 'XXX', 'LOVE', 'LUST', 'HUMBLE', 'PRIDE', 'LOYALTY','FEEL', 'ELEMENT', 'YAH', 'DNA', 'BLOOD']
+*/
 
 /**
  * @param {array} tracks
