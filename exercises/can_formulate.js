322eaa0268186c08fcd5422c6550f038d21deeef
alohaglenn <glenngonda@gmail.com>
2017-12-10 22:40:24 -0800

    chore: update directions for can_formulate

--- exercises/can_formulate.js
+++ exercises/can_formulate.js
@@ -3,7 +3,17 @@ Given an arbitrary target string and another string of lyrics, write a function 
 
 Each letter in the lyrics string can only be used once in your target string.
 
-Note: Ignore case-sensitivity.
+Note: Assume both inputs will be valid strings.
+
+Examples:
+
+let target = 'Hack Reactor'
+let good_kid_chorus = 'Mass hallucination baby Ill education baby Want to reconnect with your elations This is your station baby';
+can_formulate(target, good_kid_chorus); => return false
+
+let target = 'Kendrick Lamar';
+let now_or_never_verse = 'Waking up in a dream Sleepwalking on another big stage You never heard peace til you hear people scream Your name in unison';
+can_formulate(target, now_or_never_verse); => return true
 */
 
 // Example:
