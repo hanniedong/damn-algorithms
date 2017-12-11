61cd49c78872dc57d2f31ef48884a53fabbf7dd5
alohaglenn <glenngonda@gmail.com>
2017-12-11 01:07:54 -0800

    chore: update examples for stack

--- exercises/stack.js
+++ exercises/stack.js
@@ -1,5 +1,4 @@
 /*
-
 Create a Stack class WITHOUT implementing any JavaScript array methods (Array.push, Array.pop, etc).
 
 The stack should have the following methods:
@@ -9,6 +8,17 @@ peek - tells us what value was last added to the Stack
 getSize - tells us how many values are currently in the stack
 reset - removes all elements from the stack
 
+Examples:
+
+stack.push('good');
+stack.push('kid');
+stack.storage = { 0: 'good', 1: 'kid'}
+stack.peek() => return 'kid'
+stack.pop() => return 'kid'
+stack.storage = { 0: 'good'}
+stack.getSize => return 1
+stack.reset()
+stack.storage = {}
  */
 
 class Stack {
