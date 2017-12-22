f1178a6166d62fc3fb47df112d8940470619a32b
alohaglenn <glenngonda@gmail.com>
2017-12-21 23:58:15 -0800

    refactor: use recursion instead of while loops

--- solutions/data_structures/binary_search_tree.js
+++ solutions/data_structures/binary_search_tree.js
@@ -15,78 +15,65 @@ class BinarySearchTree {
     this._root = null;
   }
 
-  add(value) {
-    const node = new Node(value);
-
-    if (this._root === null) {
-      this._root = node;
-    } else {
-      let curr_node = this._root;
-
-      while (curr_node) {
-        if (curr_node.value > value) {
-          if (curr_node.left === null) {
-            curr_node.left = node;
-            return;
-          } else {
-            curr_node = curr_node.left;
-          }
-        } else {
-          if (curr_node.right === null) {
-            curr_node.right = node;
-            return;
-          } else {
-            curr_node = curr_node.right;
-          }
-        }
+  add(value, node = this._root) {
+    if (!this._root) {
+      this._root = new Node(value);
+      return;
+    } else if (value <= node.value) {
+      if (!node.left) {
+        node.left = new Node(value);
+        return;
+      } else {
+        return this.add(value, node.left);
+      }
+    } else if (value > node.value) {
+      if (!node.right) {
+        node.right = new Node(value);
+        return;
+      } else {
+        return this.add(value, node.right);
       }
     }
   }
 
-  contains(value) {
-    let curr_node = this._root;
+  contains(value, node = this._root) {
+    if (!this._root) {
+      return false;
+    }
 
-    while (curr_node) {
-      if (curr_node.value === value) {
-        return true;
-      }
-
-      if (curr_node.value > value) {
-        curr_node = curr_node.left;
-      } else {
-        curr_node = curr_node.right;
-      }
+    if (value === node.value) {
+      return true;
+    } else if (value <= node.value && node.left) {
+      return this.contains(value, node.left);
+    } else if (value > node.value && node.right) {
+      return this.contains(value, node.right);
     }
 
     return false;
   }
 
-  get_min() {
-    if (this._root === null) {
+  get_min(node = this._root) {
+    if (!this._root) {
       return null;
     }
 
-    let curr_node = this._root;
-
-    while (curr_node.left) {
-      curr_node = curr_node.left;
+    if (node.left) {
+      return this.get_min(node.left);
     }
 
-    return curr_node.value;
+    return node.value;
   }
 
-  get_max() {
-    if (this._root === null) {
+  get_max(node = this._root) {
+    if (!this._root) {
       return null;
     }
 
-    let curr_node = this._root;
-
-    while (curr_node.right) {
-      curr_node = curr_node.right;
+    if (node.right) {
+      return this.get_max(node.right);
     }
 
-    return curr_node.value;
+    return node.value;
   }
 }
 
