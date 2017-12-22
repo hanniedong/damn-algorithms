be67af3df04713b4c86e8c012b8c722f2f42294f
alohaglenn <glenngonda@gmail.com>
2017-12-22 00:21:59 -0800

    fix: instance should be bst

--- __tests__/data_structures/binary_search_tree.js
+++ __tests__/data_structures/binary_search_tree.js
@@ -8,6 +8,6 @@ test('Node is a class', () => {
 
 test('BinarySearchTree is a class', () => {
   expect(typeof BinarySearchTree).toEqual('function');
-  const ll = new BinarySearchTree();
-  expect(ll).toBeInstanceOf(BinarySearchTree);
+  const bst = new BinarySearchTree();
+  expect(bst).toBeInstanceOf(BinarySearchTree);
 });
