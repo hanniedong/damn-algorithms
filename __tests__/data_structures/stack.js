427bf5ca66f2f958548aed68b94c97e33d8837fd
alohaglenn <glenngonda@gmail.com>
2017-12-23 00:58:49 -0800

    refactor: use describe blocks to segment tests

--- __tests__/data_structures/stack.js
+++ __tests__/data_structures/stack.js
@@ -1,112 +1,123 @@
 import Stack from '../../exercises/data_structures/stack';
 
-test('Stack is a function', () => {
-  expect(typeof Stack).toEqual('function');
+describe('ES6 Class', () => {
+  test('has a Stack class', () => {
+    expect(typeof Stack).toEqual('function');
+    const stack = new Stack();
+    expect(stack).toBeInstanceOf(Stack);
+  });
 });
 
-test('stack is an instance of Stack', () => {
-  const stack = new Stack();
-  expect(stack).toBeInstanceOf(Stack);
+describe('Stack constructor', () => {
+  test('has a size property that initialized to 0', () => {
+    const stack = new Stack();
+    expect(stack._size).toEqual(0);
+  });
+
+  test('Stack has a storage property that is initialized to an empty object', () => {
+    const stack = new Stack();
+    expect(stack._storage).toMatchObject({});
+  });
 });
 
-test('Stack has a size property that is a number', () => {
-  const stack = new Stack();
-  expect(typeof stack._size).toEqual('number');
+describe('push', () => {
+  test('is a method', () => {
+    const stack = new Stack();
+    expect(typeof stack.push).toEqual('function');
+  });
+
+  test('adds a value to the top of the stack', () => {
+    const stack = new Stack();
+    stack.push('good');
+    expect(stack._storage).toMatchObject({ '0': 'good' });
+    stack.push('kid');
+    expect(stack._storage).toMatchObject({ '0': 'good', '1': 'kid' });
+    stack.push('maad');
+    expect(stack._storage).toMatchObject({ '0': 'good', '1': 'kid', '2': 'maad' });
+    stack.push('city');
+    expect(stack._storage).toMatchObject({ '0': 'good', '1': 'kid', '2': 'maad', '3': 'city' });
+  });
 });
 
-test('Stack has a storage property that is an object', () => {
-  const stack = new Stack();
-  expect(typeof stack._storage).toEqual('object');
+describe('pop', () => {
+  test('is a method', () => {
+    const stack = new Stack();
+    expect(typeof stack.pop).toEqual('function');
+  });
+
+  test('removes the value at the top of stack', () => {
+    const stack = new Stack();
+    stack.push('good');
+    stack.push('kid');
+    stack.push('maad');
+    stack.push('city');
+    stack.pop();
+    expect(stack._storage).toMatchObject({ '0': 'good', '1': 'kid', '2': 'maad' });
+    stack.pop();
+    expect(stack._storage).toMatchObject({ '0': 'good', '1': 'kid' });
+    stack.pop();
+    expect(stack._storage).toMatchObject({ '0': 'good' });
+    stack.pop();
+    expect(stack._storage).toMatchObject({});
+  });
 });
 
-test('Stack has a push method', () => {
-  const stack = new Stack();
-  expect(typeof stack.push).toEqual('function');
+describe('peek', () => {
+  test('is a method', () => {
+    const stack = new Stack();
+    expect(typeof stack.peek).toEqual('function');
+  });
+
+  test("returns the value at top of the stack, but doesn't alter storage", () => {
+    const stack = new Stack();
+    stack.push('good');
+    stack.push('kid');
+    stack.push('maad');
+    stack.push('city');
+    expect(stack.peek()).toEqual('city');
+    expect(stack.pop()).toEqual('city');
+    expect(stack.peek()).toEqual('maad');
+    expect(stack.pop()).toEqual('maad');
+    expect(stack.peek()).toEqual('kid');
+    expect(stack.pop()).toEqual('kid');
+    expect(stack.peek()).toEqual('good');
+    expect(stack.pop()).toEqual('good');
+  });
 });
 
-test('Stack push method adds item to the top of the stack', () => {
-  const stack = new Stack();
-  stack.push('good');
-  expect(stack._storage).toMatchObject({ '0': 'good' });
-  stack.push('kid');
-  expect(stack._storage).toMatchObject({ '0': 'good', '1': 'kid' });
-  stack.push('maad');
-  expect(stack._storage).toMatchObject({ '0': 'good', '1': 'kid', '2': 'maad' });
-  stack.push('city');
-  expect(stack._storage).toMatchObject({ '0': 'good', '1': 'kid', '2': 'maad', '3': 'city' });
+describe('get_size', () => {
+  test('is a method', () => {
+    const stack = new Stack();
+    expect(typeof stack.get_size).toEqual('function');
+  });
+
+  test('retuns the correct number of values in storage', () => {
+    const stack = new Stack();
+    expect(stack.get_size()).toEqual(0);
+    stack.push('good');
+    expect(stack.get_size()).toEqual(1);
+    stack.push('kid');
+    expect(stack.get_size()).toEqual(2);
+    stack.pop();
+    expect(stack.get_size()).toEqual(1);
+    stack.pop();
+    expect(stack.get_size()).toEqual(0);
+  });
 });
 
-test('Stack has a pop method', () => {
-  const stack = new Stack();
-  expect(typeof stack.pop).toEqual('function');
-});
+describe('reset', () => {
+  test('is a method', () => {
+    const stack = new Stack();
+    expect(typeof stack.reset).toEqual('function');
+  });
 
-test('Stack pop method removes item at the top of stack', () => {
-  const stack = new Stack();
-  stack.push('good');
-  stack.push('kid');
-  stack.push('maad');
-  stack.push('city');
-  stack.pop();
-  expect(stack._storage).toMatchObject({ '0': 'good', '1': 'kid', '2': 'maad' });
-  stack.pop();
-  expect(stack._storage).toMatchObject({ '0': 'good', '1': 'kid' });
-  stack.pop();
-  expect(stack._storage).toMatchObject({ '0': 'good' });
-  stack.pop();
-  expect(stack._storage).toMatchObject({});
-});
-
-test('Stack has a peek method', () => {
-  const stack = new Stack();
-  expect(typeof stack.peek).toEqual('function');
-});
-
-test("Stack peek method returns value at top of the stack, but doesn't alter storage", () => {
-  const stack = new Stack();
-  stack.push('good');
-  stack.push('kid');
-  stack.push('maad');
-  stack.push('city');
-  expect(stack.peek()).toEqual('city');
-  expect(stack.pop()).toEqual('city');
-  expect(stack.peek()).toEqual('maad');
-  expect(stack.pop()).toEqual('maad');
-  expect(stack.peek()).toEqual('kid');
-  expect(stack.pop()).toEqual('kid');
-  expect(stack.peek()).toEqual('good');
-  expect(stack.pop()).toEqual('good');
-});
-
-test('Stack has a get_size method', () => {
-  const stack = new Stack();
-  expect(typeof stack.get_size).toEqual('function');
-});
-
-test('Stack get_size method retuns number of values in storage', () => {
-  const stack = new Stack();
-  expect(stack.get_size()).toEqual(0);
-  stack.push('good');
-  expect(stack.get_size()).toEqual(1);
-  stack.push('kid');
-  expect(stack.get_size()).toEqual(2);
-  stack.pop();
-  expect(stack.get_size()).toEqual(1);
-  stack.pop();
-  expect(stack.get_size()).toEqual(0);
-});
-
-test('Stack has a reset method', () => {
-  const stack = new Stack();
-  expect(typeof stack.reset).toEqual('function');
-});
-
-test('Stack reset method empties the storage object', () => {
-  const stack = new Stack();
-  stack.push('good');
-  stack.push('kid');
-  stack.push('maad');
-  stack.push('city');
-  stack.reset();
-  expect(stack._storage).toMatchObject({});
+  test('empties the storage object', () => {
+    const stack = new Stack();
+    stack.push('good');
+    stack.push('kid');
+    stack.push('maad');
+    stack.push('city');
+    stack.reset();
+    expect(stack._storage).toMatchObject({});
+  });
 });
