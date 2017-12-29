5f8458b61fd510d87d0de528122fd1fd81a5ab6b
alohaglenn <glenngonda@gmail.com>
2017-12-28 22:08:45 -0800

    chore: add instructions and example for binary_search_tree

--- exercises/data_structures/binary_search_tree.js
+++ exercises/data_structures/binary_search_tree.js
@@ -1,5 +1,24 @@
-/* 
-BINARY SEARCH TREE
+/*
+Implement a binary search tree using two classes. The Node class should take in a value and track the children nodes to its left and right. The Binary Search Tree class should have a reference to its root.
+
+The Binary Search Tree class should also have the following methods:
+add - takes in a value and a node and appends a new node to the tree in the appropriate place
+contains - traverses the tree and returns a boolean based on if a given value is present
+get_min - traverses the tree and returns the minimum value in the tree
+get_max -traverses the tree and returns the maximum value in the tree
+
+Example:
+const bst = new BinarySearchTree();
+let values = [1,-1,0,-2,-3,4,2];
+values.forEach(val => bst.add(val));
+bst.contains(-2); => return true
+bst.contains(3); => return false
+bst.get_min(); => return -3
+bst.get_max(); => return 4
+bst.add(-5);
+bst.add(7);
+bst.get_min(); => return -5
+bst.get_max(); => return 7
 */
 
 class Node {}
