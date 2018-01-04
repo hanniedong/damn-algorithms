a0c6deab7692ca810f559d25ab1b8dab91b02688
alohaglenn <glenngonda@gmail.com>
2018-01-04 01:26:18 -0800

    feat: add more unit tests for binary_search

--- __tests__/algorithms/binary_search.test.js
+++ __tests__/algorithms/binary_search.test.js
@@ -1,4 +1,4 @@
-import binary_search from '../../solutions/algorithms/binary_search';
+import binary_search from '../../exercises/algorithms/binary_search';
 
 describe('binary_search', () => {
   test('is a function', () => {
@@ -6,8 +6,34 @@ describe('binary_search', () => {
     expect(result).toEqual('function');
   });
 
-  test('returns -1 if the target_value is not found in a given list', () => {
-    const result = binary_search([-15, 1, 3, 6, 23, 24, 32, 34, 41, 99], 83);
+  test('returns -1 if the list is empty', () => {
+    const result = binary_search([], 5);
     expect(result).toEqual(-1);
+  });
+
+  test('returns -1 if the target_value is not found in a given sorted list', () => {
+    const a = binary_search([8, 13, 22, 24, 25, 32, 33, 34, 42, 44, 52], 2);
+    expect(a).toEqual(-1);
+    const b = binary_search([1, 2, 3, 4, 5], 0);
+    expect(b).toEqual(-1);
+    const c = binary_search([6, 7, 8, 9, 10], 11);
+    expect(c).toEqual(-1);
+    const d = binary_search(['a', 'b', 'c', 'd', 'e'], 'f');
+    expect(d).toEqual(-1);
+    const e = binary_search(['good', 'kid', 'maad'], 'city');
+    expect(e).toEqual(-1);
+  });
+
+  test('returns the index of the target_value if it is found in a given sorted list', () => {
+    const a = binary_search([8, 13, 22, 24, 25, 32, 33, 34, 42, 44, 52], 24);
+    expect(a).toEqual(3);
+    const b = binary_search([1, 2, 3, 4, 5], 5);
+    expect(b).toEqual(4);
+    const c = binary_search([6, 7, 8, 9, 10], 6);
+    expect(c).toEqual(0);
+    const d = binary_search(['a', 'b', 'c', 'd', 'e'], 'd');
+    expect(d).toEqual(3);
+    const e = binary_search(['good', 'kid', 'maad'], 'kid');
+    expect(e).toEqual(1);
   });
 });
