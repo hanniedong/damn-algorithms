a5f592f9ad9b9409597cdf3a97bfa59553e4f0f1
alohaglenn <glenngonda@gmail.com>
2018-01-04 23:47:26 -0800

    chore: update directions for reverse_tracks_in_place

--- exercises/algorithms/reverse_tracks_in_place.js
+++ exercises/algorithms/reverse_tracks_in_place.js
@@ -1,10 +1,11 @@
 /*
 Eight months after "DAMN" was released, Kendrick Lamar released "DAMN COLLECTOR'S EDITION" which contained the exact same tracks, but with the album order reversed.
 
-Given an array of track names as strings, reverse the elements in the array in-place and without using JavaScript's Array.reverse method.
+Given an array of track names as strings, reverse the elements in the array in-place.
+
+You must modify the input array on the fly. Don't create any additional arrays and don't use JavaScript's Array.prototype.reverse() method.
 
 Example:
-
 let album_tracks = ['BLOOD', 'DNA', 'YAH', 'ELEMENT', 'FEEL', 'LOYALTY', 'PRIDE', 'HUMBLE', 'LUST', 'LOVE', 'XXX', 'FEAR', 'GOD', 'DUCKWORTH'];
 reverse_tracks_in_place(album_tracks) => return ['DUCKWORTH', 'GOD', 'FEAR', 'XXX', 'LOVE', 'LUST', 'HUMBLE', 'PRIDE', 'LOYALTY','FEEL', 'ELEMENT', 'YAH', 'DNA', 'BLOOD']
 */
