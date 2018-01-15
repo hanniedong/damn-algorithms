7d99be4d431faa608ed8457e3216c826d7433f5b
alohaglenn <glenngonda@gmail.com>
2018-01-15 01:58:14 -0800

    Revert "chore: update directions"
    This reverts commit af24337ce337bc3b763b100efbda9c487c2983da.

--- exercises/04_remove_track_instances.js
+++ exercises/04_remove_track_instances.js
@@ -1,5 +1,5 @@
 /*
-Given an array of tracks and a target track, remove all instances of that target track in the array and return the new length of the array. Perform the removal of the target in-place without creating extra space for another array and without using the Array.prototype.reverse method.
+Given an array of tracks and a target track, remove all instances of that target track in the array and return the new length of the array. Perform the removal of the target in-place without creating extra space for another array.
 
 Example:
 const damn = ['DNA', 'HUMBLE', 'LOVE', 'DNA', 'LOYALTY', 'DNA', 'DNA'];
